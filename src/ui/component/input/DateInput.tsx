fb33bf4bbde5dd9f65e4ca28af8040dfa4f8431f
Aleksandras Petrovicevas <aleksandras.petrovicevas@nfq.lt>
2023-04-09 14:05:17 +0300

    feat: form

---
+++ src/ui/component/input/DateInput.tsx
@@ -0,0 +1,83 @@
+import { COLOR_GREY, COLOR_SECONDARY, COLOR_WHITE } from '@color';
+import { CalendarIcon } from '@component/icon/CalendarIcon';
+import { useFormikContext } from 'formik';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+
+interface DetailsValues {
+  carNumber: string;
+  date: Date;
+  personalCode: string;
+}
+
+export const DateInput: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const { setFieldValue, values } = useFormikContext<DetailsValues>();
+  const { date } = values;
+
+  return (
+    <TouchableWithoutFeedback onPress={() => setOpen(true)}>
+      <View pointerEvents="box-only" style={styles.inputContainer}>
+        <Text style={styles.label}>Draudimo įsigaliojimo laikas</Text>
+        <View style={styles.row}>
+          <TextInput
+            editable={false}
+            style={styles.input}
+            value={date.toISOString().slice(0, 10).replace(/-/g, ' ')}
+          />
+          <CalendarIcon />
+        </View>
+        <DatePicker
+          modal
+          date={date}
+          mode="date"
+          open={open}
+          minimumDate={new Date()}
+          onConfirm={date => {
+            setOpen(false);
+            setFieldValue('date', date);
+          }}
+          onCancel={() => setOpen(false)}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    color: COLOR_SECONDARY,
+    flex: 1,
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 16,
+    width: '100%',
+  },
+  inputContainer: {
+    alignItems: 'flex-start',
+    backgroundColor: COLOR_WHITE,
+    borderRadius: 8,
+    height: 80,
+    justifyContent: 'space-between',
+    marginBottom: 4,
+    marginTop: 32,
+    padding: 16,
+    width: '100%',
+    zIndex: 10,
+  },
+  label: {
+    color: COLOR_GREY,
+    fontFamily: 'Poppins-Regular',
+    fontSize: 12,
+  },
+  row: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
--- src/ui/screen/DetailsScreen.tsx
+++ src/ui/screen/DetailsScreen.tsx
@@ -1,8 +1,47 @@
-import {Text, View} from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import React from 'react';
+import { Title } from '@component/text/Title';
+import { Subtitle } from '@component/text/Subtitle';
+import { DetailsForm } from '@component/form/DetailsForm';
 
 export const DetailsScreen: React.FC = () => (
-  <View>
-    <Text>DetailsScreen</Text>
-  </View>
+  <KeyboardAvoidingView
+    style={styles.flex1}
+    behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ScrollView style={styles.flex1} keyboardShouldPersistTaps={'handled'}>
+        <View style={styles.detailsContainer}>
+          <View style={styles.titleContainer}>
+            <Title>Automobilio Draudimas</Title>
+          </View>
+          <Subtitle>
+            Duis sodales urna aliquam, aliquam arcu vehicula, pharetra felis
+          </Subtitle>
+          <DetailsForm />
+        </View>
+      </ScrollView>
+    </TouchableWithoutFeedback>
+  </KeyboardAvoidingView>
 );
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '93%',
+  },
+  flex1: {
+    flex: 1,
+  },
+  titleContainer: {
+    marginBottom: 8,
+  },
+});
